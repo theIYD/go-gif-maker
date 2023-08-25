85a73c0cb22b0173a7e921c84ea9685d01008f4d
Idrees <idrees@zomentum.com>
2023-08-25 09:18:49 +0530

    feat: use ffmpeg

--- main.go
+++ main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
+	"strings"
+
+	ffmpeg "github.com/u2takey/ffmpeg-go"
 )
 
 func errorHandler(err error) {
@@ -19,13 +23,24 @@ func main() {
 	path := flag.String("path", currentDir, "The path to images")
 	flag.Parse()
 
+	if _, err := os.Stat("./output"); os.IsNotExist(err) {
+		// create output dir
+		err = os.Mkdir("output", 0755)
+		errorHandler(err)
+	}
+
 	// read path directory
-	dir, err := os.ReadDir(*path)
+	err = os.Chdir(*path)
 	errorHandler(err)
 
-	var fileNames []string
+	dir, err := os.ReadDir(".")
+	errorHandler(err)
 
-	for _, file := range dir {
-		fileNames = append(fileNames, file.Name())
-	}
+	// get the file name
+	filePattern := strings.Split(dir[0].Name(), "-")
+	inputFileName := fmt.Sprintf("%s-%%d.png", filePattern[0])
+	outputFilePath := fmt.Sprintf("../output/%s-final.gif", filePattern[0])
+
+	err = ffmpeg.Input(inputFileName, ffmpeg.KwArgs{"f": "image2", "framerate": "10", "loop": "0"}).Output(outputFilePath).OverWriteOutput().ErrorToStdOut().Run()
+	errorHandler(err)
 }
